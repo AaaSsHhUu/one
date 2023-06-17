a5da160d9d0b19bb1b394242f5152936d2a12495
Ashu <anegi6723@gmail.com>
2023-06-17 12:27:56 +0530

    Made Footer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Footer from './components/Footer';
 import './style/App.scss';
 import './style/Header.scss'
 import './style/Home.scss'
+import './style/footer.scss'
 
 
 export default function App() {
@@ -14,6 +16,7 @@ export default function App() {
         <Routes>
             <Route path='/' element={<Home/>} />
         </Routes>
+        <Footer />
     </Router>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function () {
+  return (
+    <footer>
+        <div>
+            <h1>Techystar</h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us On</h5>
+            <div>
+            <a href="https://youtube.com" target={"_blank"}>
+                Youtube
+            </a>
+            <a href="https://instagram.com" target={"_blank"}>
+                Instagram
+            </a>
+            <a href="https://github.com/AaaSsHhUu/one.git" target={"_blank"}>
+                Github
+            </a>
+            </div>
+        </div>
+    </footer>
+  );
+}
