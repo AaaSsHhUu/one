9880474a869c23cb6e17ce001dc8618e80946e55
Ashu <anegi6723@gmail.com>
2023-06-18 09:37:11 +0530

    Createed Contact Page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
 import './style/App.scss';
-import './style/Header.scss'
-import './style/Home.scss'
-import './style/footer.scss'
+import './style/Header.scss';
+import './style/Home.scss';
+import './style/footer.scss';
+import './style/contact.scss';
 
 
 export default function App() {
@@ -15,6 +17,7 @@ export default function App() {
         <Header />
         <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='/contact' element={<Contact />}/>
         </Routes>
         <Footer />
     </Router>
---
+++ src/components/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className='contact'>
+        <main>
+            <h1>Contact Us</h1>
+            <form>
+                <div>
+                    <label>Name :</label>
+                    <input type="text" required placeholder='Enter Your Name' />
+                </div>
+
+                <div>
+                    <label>Email :</label>
+                    <input type='email' required placeholder='Enter Your Email' />
+                </div>
+
+                <div>
+                    <label>Message :</label>
+                    <input type="text" required placeholder='Write Your Query...' />
+                </div>
+
+                <button type="submit">Send</button>
+            </form>
+        </main>
+    </div>
+  )
+}
